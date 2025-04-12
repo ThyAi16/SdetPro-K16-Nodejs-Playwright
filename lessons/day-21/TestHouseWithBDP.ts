0ab9d68faac809c71ab039a4709e5b662fe35c94
Thy Ai <ainguyen@192.168.2.11>
2025-04-12 18:45:01 +0700

    lesson 21

--- lessons/day-21/TestHouseWithBDP.ts
+++ lessons/day-21/TestHouseWithBDP.ts
@@ -1,9 +1,18 @@
 import House from "./HouseWithBuilderDesignPattern";
 
 let builder = new House.Builder();
-let house: House = builder
-  .setTopRoofColor("Red")
-  .setWindowNumber(2)
-  .setColor("Yellow")
-  .setmainDoorNumber(9)
-  .build();
+
+// Option 1:
+// let house: House = builder
+//   .setTopRoofColor("Red")
+//   .setWindowNumber(2)
+//   .setColor("Yellow")
+//   .setmainDoorNumber(9)
+//   .build();
+
+// Option 2:
+builder.setTopRoofColor("Red");
+builder.setWindowNumber(2);
+builder.setColor("Yellow");
+builder.setmainDoorNumber(9);
+let house: House = builder.build();
